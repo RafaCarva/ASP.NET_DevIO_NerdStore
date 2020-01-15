fa3a077531f244ffc42aab18c8f3cf710a01b1a8
Rafael Carvalho <rafael.faria.carvalho@gmail.com>
2020-01-14 23:00:37 -0300

    Entity framework Add

--- NerdStore/src/NerdStore.Catalogo.Domain/Categoria.cs
+++ NerdStore/src/NerdStore.Catalogo.Domain/Categoria.cs
@@ -9,6 +9,14 @@ namespace NerdStore.Catalogo.Domain
     {
         public string Nome { get; private set; }
         public int Codigo { get; private set; }
+        
+        // EF Relation
+        public ICollection<Produto> Produtos { get; set; }
+
+        protected Categoria()
+        {
+
+        }
 
         public Categoria(string nome, int codigo)
         {
